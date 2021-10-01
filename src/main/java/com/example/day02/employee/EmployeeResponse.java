e7eff6f5ca1a1fab34f7f50ed22cc8df823d782a
NAWAPHONG ONCHARUN (CDGS-ADD) <nawaphong.o@cdg.co.th>
2021-10-01 14:14:53 +0700

    add employee controller

---
+++ src/main/java/com/example/day02/employee/EmployeeResponse.java
@@ -0,0 +1,15 @@
+package com.example.day02.employee;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public class EmployeeResponse {
+	@Getter @Setter private int id;
+	@Getter @Setter private String name;
+	
+
+	EmployeeResponse(int id,String name){
+		this.id = id;
+		this.name = name;
+	}
+}
